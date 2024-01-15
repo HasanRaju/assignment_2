efb51ca17d957d48b56e935f4dcccd4e51b68761
maksudul.raju <maksudul.raju@bol-online.com>
2024-01-15 16:28:03 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,228 +7,175 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ShoppingBag(),
+      home: ShoppingCart(),
     );
   }
 }
 
-class ShoppingBag extends StatefulWidget {
+class Product {
+  final String name;
+  final String color;
+  final String size;
+  final double price;
+  int quantity;
+
+  Product({
+    required this.name,
+    required this.color,
+    required this.size,
+    required this.price,
+    this.quantity = 1,
+  });
+}
+
+class ShoppingCart extends StatefulWidget {
+  const ShoppingCart({Key? key}) : super(key: key);
+
   @override
-  _ShoppingBagState createState() => _ShoppingBagState();
+  _ShoppingCartState createState() => _ShoppingCartState();
 }
 
-class _ShoppingBagState extends State<ShoppingBag> {
-  Map<String, int> items = {
-    'Pullover': 1,
-    'T-Shirt': 1,
-    'Sport Dress': 1,
-  };
+class _ShoppingCartState extends State<ShoppingCart> {
+  final List<Product> products = [
+    Product(
+      name: 'Pullover',
+      color: 'Black',
+      size: 'L',
+      price: 51.0,
+    ),
+    Product(
+      name: 'T-Shirt',
+      color: 'Gray',
+      size: 'L',
+      price: 30.0,
+    ),
+    Product(
+      name: 'Sport Dress',
+      color: 'Black',
+      size: 'M',
+      price: 43.0,
+    ),
+  ];
 
-  Map<String, int> prices = {
-    'Pullover': 51,
-    'T-Shirt': 30,
-    'Sport Dress': 43,
-  };
+  double get totalAmount => products.fold(
+    0,
+        (previousValue, element) => previousValue + element.price * element.quantity,
+  );
 
-  Map<String, bool> isDisabled = {
-    'Pullover': false,
-    'T-Shirt': false,
-    'Sport Dress': false,
-  };
-
-  void _showDialog(String itemName, int quantity) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Congratulations!'),
-          content: Text('You have added $quantity $itemName on your bag!'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('OKAY'),
-              onPressed: () { Navigator.of(context).pop(); },
-            ),
-          ],
+  void incrementQuantity(int index) {
+    setState(() {
+      if (products[index].quantity < 5) {
+        products[index].quantity++;
+      } else {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Congratulations!'),
+            content: Text('You have added 5 ${products[index].name} on your bag!'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OKAY'),
+              ),
+            ],
+          ),
         );
-      },
-    );
+      }
+    });
   }
 
-  void _showSnackBar() {
-    final snackBar = SnackBar(content: Text('Congratulations on checking out!'));
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  void decrementQuantity(int index) {
+    setState(() {
+      if (products[index].quantity > 1) {
+        products[index].quantity--;
+      }
+    });
   }
-
-  int get totalAmount => items.keys.fold(0, (sum, key) => sum + (prices[key] ?? 0) * (items[key] ?? 0));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          onPressed: () {},
-        ),
-        title: Text(
-          'My Bag',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
+        title: Text('My Bag'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (BuildContext context, int index) {
-                String key = items.keys.elementAt(index);
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        key,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.remove),
-                            onPressed: () {
-                              setState(() {
-                                if (items[key] != null && items[key]! > 0) {
-                                  items[key] = (items[key] ?? 0) - 1;
-                                  isDisabled[key] = false;
-                                }
-                              });
-                            },
-                          ),
-                          Text(
-                            '${items[key] ?? 0}',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.add),
-                            onPressed: () {
-                              setState(() {
-                                if (items[key] == 5) {
-                                  isDisabled[key] = true;
-                                }
-                                if (items[key] != null && items[key]! < 5) {
-                                  items[key] = (items[key] ?? 0) + 1;
-                                  if (items[key] == 5) {
-                                    _showDialog(key, items[key]!);
-                                  }
-                                }
-                              });
-                            },
-                            disabledColor: Colors.grey,
-                            //disabledTextColor: Colors.white,
-                            color: Colors.blue,
-                            //textColor: Colors.white,
-                            iconSize: 24,
-                            padding: EdgeInsets.all(16),
-                            splashRadius: 24,
-                            tooltip: 'Add',
-                            splashColor: Colors.blueAccent,
-                            highlightColor: Colors.blueAccent,
-                            //disabled: isDisabled[key],
-                          ),
-                          Text(
-                            '\$${prices[key]! * items[key]!}',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Icon(Icons.shopping_cart),
+              title: Text(products[index].name),
+              subtitle: Text('${products[index].color} / ${products[index].size}'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () => decrementQuantity(index),
+                    icon: Icon(Icons.remove),
                   ),
-                );
-              },
+                  Text('${products[index].quantity}'),
+                  IconButton(
+                    onPressed: () => incrementQuantity(index),
+                    icon: Icon(Icons.add),
+                  ),
+                  Text('\$${(products[index].price * products[index].quantity).toStringAsFixed(2)}'),
+                ],
+              ),
             ),
-          ),
-          Container(
-            height: 80,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
+          );
+
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    'Total amount',
+                    'Total amount:',
                     style: TextStyle(
-                      fontSize: 16,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                   Text(
-                    '\$${totalAmount}',
+                    '${totalAmount.toStringAsFixed(2)}\$',
                     style: TextStyle(
-                      fontSize: 16,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
               ),
-
-            ),
-          ),
-          // Button at the bottom
-          //SizedBox(height: 20), // Add spacing
-          SizedBox(
-            width: double.infinity, // Full width
-            child: ElevatedButton(
-              onPressed: _showSnackBar,
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red, // Set the button color to red
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0), // Set the button's border radius
+              SizedBox(height: 24),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Congratulations!'),
+                      ),
+                    );
+                  },
+                  child: Text('CHECK OUT'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0), // Set the button's border radius
+                    ),
+                  ),
                 ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text(
-                  'CHECK OUT',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-            ),
+            ],
           ),
-
-
-        ],
+        ),
       ),
 
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _showSnackBar,
-      //   child: Icon(Icons.check),
-      // ),
+
+
     );
   }
 }
